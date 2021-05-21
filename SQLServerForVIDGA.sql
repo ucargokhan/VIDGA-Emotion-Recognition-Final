ddac7e7147ef459880991b324c676e13e45351c0
Gökhan Uçar <gokhan.gucar@gmail.com>
2021-05-22 00:03:51 +0300

    Local Database Added

---
+++ SQLServerForVIDGA.sql
@@ -0,0 +1,115 @@
+USE [master]
+GO
+
+/****** Object:  Database [VidgaEmotionRecognition]    Script Date: 21.05.2021 23:52:04 ******/
+CREATE DATABASE [VidgaEmotionRecognition]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'VidgaEmotionRecognition', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS1\MSSQL\DATA\VidgaEmotionRecognition.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'VidgaEmotionRecognition_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS1\MSSQL\DATA\VidgaEmotionRecognition_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [VidgaEmotionRecognition].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [VidgaEmotionRecognition] SET  READ_WRITE 
+GO
+
